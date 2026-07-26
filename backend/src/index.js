d3aff5d1a943c3d315ba631173a2008090cf98a0
Somu Mohan Reddy <mohanreddysomu44@gmail.com>
2026-07-26 15:59:28 +0530

    updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,9 +51,9 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
+      process.env.FRONTEND_URL,
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://truvo-theta.vercel.app",
     ],
     credentials: true,
   }),
